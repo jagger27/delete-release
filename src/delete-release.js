4dc19198ce13ac7ab1763ff5966d186a13a6d500
Jagger De Leo <jagger@twoseven.ca>
2019-11-23 12:52:59 -0500

    Replace create-release.js with delete-release.js

--- src/delete-release.js
+++ src/delete-release.js
@@ -14,33 +14,27 @@ async function run() {
 
     // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
     const tag = tagName.replace('refs/tags/', '');
-    const releaseName = core.getInput('release_name', { required: true }).replace('refs/tags/', '');
-    const body = core.getInput('body', { required: false });
-    const draft = core.getInput('draft', { required: false }) === 'true';
-    const prerelease = core.getInput('prerelease', { required: false }) === 'true';
 
-    // Create a release
-    // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-    // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-    const createReleaseResponse = await github.repos.createRelease({
+    const getReleaseByTagResponse = await github.repos.getReleaseByTag({
       owner,
       repo,
-      tag_name: tag,
-      name: releaseName,
-      body,
-      draft,
-      prerelease
+      tag
     });
 
     // Get the ID, html_url, and upload URL for the created Release from the response
     const {
-      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl }
-    } = createReleaseResponse;
+      data: { id: releaseId }
+    } = getReleaseByTagResponse;
+
+    const deleteReleaseResponse = await github.repos.deleteRelease({
+      owner: owner,
+      repo: repo,
+      id: id
+    });
 
     // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     core.setOutput('id', releaseId);
-    core.setOutput('html_url', htmlUrl);
-    core.setOutput('upload_url', uploadUrl);
+    core.setOutput('delete_response', deleteReleaseResponse.status);
   } catch (error) {
     core.setFailed(error.message);
   }
